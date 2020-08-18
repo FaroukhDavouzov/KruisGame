126f7d1aa102c6acf00c4624e55db04ccd6012e8
arnodem <69833924+arnodem@users.noreply.github.com>
2020-08-18 22:12:47 +0200

    aboutstyle

--- components/about.js
+++ components/about.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { Button, StyleSheet, TextInput, ScrollView, ActivityIndicator, View, Alert, Text } from 'react-native';
 import firebase from '../database/firebase';
-export default function about(){
+export default function About(){
 return(
 <View>
-    <Text>
+    <Text style={styles.textStyle}>
         Welcome to the About screen
     </Text>
 </View>    
 )    
 }
+
+const styles=StyleSheet.create({
+    textStyle:{
+        fontSize:30
+    }
+})
